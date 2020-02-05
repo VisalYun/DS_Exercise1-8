5a57fe5eadf846edd853d6053cbd3e9cf986b8e5
Vireak_Sok <sokvireak2000@gmail.com>
2020-02-05 09:57:42 +0700

    updated Singly Linked list

--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -25,9 +25,8 @@ class SLinkedList:
                 + " " + printval.dataval.phoneNum)
             printval = printval.nextval
 
-    def insertAtBegining(self, newData):
+    def insertAtBeginning(self, newData):
         NewNode = Node(newData)
-        # Update the new nodes next val to existing node
         NewNode.nextval = self.headval
         self.headval = NewNode
 
@@ -40,6 +39,12 @@ class SLinkedList:
         while (laste.nextval):
             laste = laste.nextval
         laste.nextval = NewNode
+
+    def deleteAtBeginning(self):
+        if not self.headval:
+            return None
+        self.headval = self.headval.nextval
+        return self.headval
 
     def deleteAtEnd(self):
 
@@ -67,7 +72,7 @@ class SLinkedList:
     def stackImplement(self):
         isTrue1 = True
         while isTrue1:
-            print("1:Push operation \n2:Pop operation \n3:Display \n4:Exit")
+            print("\n+++++ Stack Menu +++++ \n1:Push operation \n2:Pop operation \n3:Display \n4:Exit")
             inp1 = int(input("Enter choice: "))
             if inp1 == 1:
                 element = list.createObject()
@@ -81,17 +86,34 @@ class SLinkedList:
             else:
                 print("Invalid input...")
 
+    def queueImplement(self):
+        isTrue2 = True
+        while isTrue2:
+            print("\n+++++ Queue Menu +++++ \n1:Push operation at end \n2:Pop operation at beginning \n3:Display "
+                  "\n4:Exit")
+            inp2 = int(input("Enter choice: "))
+            if inp2 == 1:
+                element = list.createObject()
+                list.insertAtEnd(element)
+            elif inp2 == 2:
+                list.deleteAtBeginning()
+            elif inp2 == 3:
+                list.listprint()
+            elif inp2 == 4:
+                isTrue2 = False
+            else:
+                print("Invalid input...")
 isTrue = True
 list = SLinkedList()
 while isTrue:
-    print("1:Create SLL of Student Nodes \n2:DisplayStatus \n3:InsertAtEnd \n4:DeleteAtEnd \n5:Stack Demo using SLL("
-          "Insertion and Deletion at Front) \n6:Exit")
+    print("\n ===== Main Menu ===== \n1:Create SLL of Student Nodes \n2:DisplayStatus \n3:InsertAtEnd \n4:DeleteAtEnd "
+          "\n5:Stack Demo using SLL(Insertion and Deletion at Front) \n6:Queue Demo using SLL \n7:Exit")
     inp = int(input("Enter choice: "))
     if inp == 1:
         studentNum = int(input("Enter number of student: "))
         while studentNum > 0:
             element = list.createObject()
-            list.insertAtBegining(element)
+            list.insertAtBeginning(element)
             studentNum -= 1
     elif inp == 2:
         list.listprint()
@@ -106,11 +128,8 @@ while isTrue:
     elif inp == 5:
         list.stackImplement()
     elif inp == 6:
+        list.queueImplement()
+    elif inp == 7:
         isTrue = False
     else:
         print("Wrong Input...")
-
-# 2 t kh 3 234
-# 1 r kh 3 123
-# 4 u kh 3 456
-# 5 i kh 3 567
